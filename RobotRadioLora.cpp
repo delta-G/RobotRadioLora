f7ee16e0b0a7083824ca69d037743645827185a0
david <david@DiscoMonster>
2020-12-27 23:25:59 -0600

    Think I found the problem.

--- RobotRadioLora.cpp
+++ RobotRadioLora.cpp
@@ -141,7 +141,7 @@ void loop()
 void handleRadioCommand(char* aCommand){
 	// Right now just ship everything to RMB
 	connectedToBase = true;  //we received a formatted command we must be connected
-	if(strcmp(aCommand, "<LGO>")){
+	if(!strcmp(aCommand, "<LGO>")){
 		rmbActive = true;
 	} else if(aCommand[1] == 'l'){
 		delay(250);
